ddf83a16754a9ae5f26eb10adc8be028f89bd4dd
innatecuriosity <116571759+innatecuriosity@users.noreply.github.com>
2022-11-04 16:04:52 +0100

    files linked, other preparations

---
+++ script.js
@@ -0,0 +1,10 @@
+//functions
+
+
+//----------------------------------MAIN--------------------------------------
+
+//create squares
+const field = document.querySelector(".field");
+field.style.cssText = "diplay:grid; width: 100%; aspect-ratio: 1/1";
+
+
