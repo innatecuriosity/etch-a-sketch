fd12a11b91c7ff08c95f938c6da0bf6b0f7f9cf0
innatecuriosity <116571759+innatecuriosity@users.noreply.github.com>
2022-11-04 22:29:37 +0100

    added color picker

--- script.js
+++ script.js
@@ -8,19 +8,42 @@ function changePixelColor(pixel) {
 //----------------------------------MAIN--------------------------------------
 
 //create squares
-let size = 100;
-let paint = "black"
+let rows = 16;
+let cols = 16;
+let paint = "black";
+let holdToggle = true;
+let paintList = ["black", "white", "purple", "blue", "green", "yellow", "orange", "red"];
+
+const paintboard = document.querySelector(".paintboard");
+
+    for (color of paintList) {
+        const paintBucket = document.createElement("div");
+        paintBucket.setAttribute("id", color);
+        paintBucket.classList= "paintbucket";
+        paintBucket.style.cssText = "background-color: "+color+";";
+
+        paintBucket.addEventListener("click", function(event) {
+            old = document.querySelector("#"+paint);
+            old.style.borderWidth = "2px"
+            paint = event.target.id;
+            paintBucket.style.borderWidth  = "8px";
+        })
+        paintboard.appendChild(paintBucket);
+    }
+
+//const rowsCount = document.querySelector("");
+//const colsCount = document.querySelector("");
 
 const field = document.querySelector(".field");
 field.style.cssText = "";
-for (let i = 0; i<size; i++){
+for (let i = 0; i<rows; i++){
     const row = document.createElement("div");
     row.classList = "row";
     row.style.cssText = "display:flex; flex:1 1 0"
     row.setAttribute("id","row"+i)
 
     // add 16 pixels per row
-    for (let j=0; j<size; j++){
+    for (let j=0; j<cols; j++){
         const pixel = document.createElement("div");
         pixel.setAttribute("id", "r"+i+"c"+j);
         pixel.classList= "pixel";
