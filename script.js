ba286b36b92fda9a77c68f86e7431160098d00fc
innatecuriosity <116571759+innatecuriosity@users.noreply.github.com>
2022-11-04 16:58:01 +0100

    canvas works!

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 //functions
 
+function changePixelColor(pixel) {
+    pixel.style.cssText = "background-color:" + paint + ";";
+    return NaN;
+}
 
 //----------------------------------MAIN--------------------------------------
 
 //create squares
+let size = 100;
+let paint = "black"
+
 const field = document.querySelector(".field");
-field.style.cssText = "diplay:grid; width: 100%; aspect-ratio: 1/1";
+field.style.cssText = "";
+for (let i = 0; i<size; i++){
+    const row = document.createElement("div");
+    row.classList = "row";
+    row.style.cssText = "display:flex; flex:1 1 0"
+    row.setAttribute("id","row"+i)
 
+    // add 16 pixels per row
+    for (let j=0; j<size; j++){
+        const pixel = document.createElement("div");
+        pixel.setAttribute("id", "r"+i+"c"+j);
+        pixel.classList= "pixel";
+        row.appendChild(pixel);
+        pixel.cssText = "grid-row-start:"+i+"; grid-column-start:"+j+";"
+        // add event listener
+        pixel.addEventListener("mouseover", function(event) {changePixelColor(event.target)})    
+        
+    }
 
+    field.appendChild(row);
+}
